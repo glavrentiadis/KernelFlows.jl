a1425e44fc08beb19e259318dfe418fc2894b721
Greg Lavrentiadis <glavrentiadis@gmail.com>
2024-09-11 11:22:59 -0700

    Update Binary Kernel Functions
     - Add exponential binary kernel function
    
     - Add group binary kernel function

--- src/kernel_functions_binary.jl
+++ src/kernel_functions_binary.jl
@@ -15,14 +15,13 @@
 # Author: Jouni Susiluoto, jouni.i.susiluoto@jpl.nasa.gov
 #
 
-
 """Linear kernel (with mean) for testing BinaryKernel
 correctness. This kernel also includes the mean, which is given as the
 log of the θ, since θ are always positive. θ[1] is the weight of the
 kernel, and θ[end] is the weight of the nugget (this is always the
 case). Therefore we have n+2 parameters for this kernel, with n the
 number of input dimensions."""
-function linear_mean(x1::AbstractVector{T}, x2::AbstractVector{T},
+function binary_linear_mean(x1::AbstractVector{T}, x2::AbstractVector{T},
                      θ::AbstractVector{T}) where T <: Real
     # Note that kernel_matrix_...() functions do not pass along the
     # last entry of θ, as that's always the nugget. Hence the θ here
@@ -33,8 +32,34 @@ function linear_mean(x1::AbstractVector{T}, x2::AbstractVector{T},
 end
 
 
-"""Linear binary kernel, but without mean"""
-function linear(x1::AbstractVector{T}, x2::AbstractVector{T},
+"""Binary linear binary kernel, but without mean"""
+function binary_linear(x1::AbstractVector{T}, x2::AbstractVector{T},
                 θ::AbstractVector{T}) where T <: Real
     θ[1] * x1' * x2
+end
+
+"""Binary group kernel, with a correlation scale σ"""
+function binary_group(x1::Union{AbstractVector{T}, T}, x2::Union{AbstractVector{T}, T}, 
+                     θ; tol=1e-6) where T <: Real
+    
+    #hyperparameters
+    σ = θ
+    #evaluate kernel matrix
+    K_grp =  norm(x1 .- x2) < tol ? σ^2 : 0.
+
+    return K_grp
+end
+
+"""Binary exponential kernel, with a correlation scale σ and lenght λ parameters"""
+function binary_exp(x1::AbstractVector{T}, x2::AbstractVector{T},
+                    θ::AbstractVector{T}) where T <: Real
+    
+    #hyperparameters
+    σ = θ[1]
+    λ = θ[2]
+    #evaluate kernel matrix
+    K_nexp = σ^2 * exp( λ * norm(x1 .- x2) )
+
+    #evaluate kernel matrix
+    return K_nexp
 end
