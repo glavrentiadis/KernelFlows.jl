e2f3cfa5f409f10da27615d82457e7878e6f8fd8
Jouni Susiluoto <hillomestari@posteo.net>
2026-07-08 10:45:08 -0700

    Update minibatching code.
    - Committing / cleaning up old edits. Moving different minibatching
      methods to dedicated files. Covariance minibatching is all-comments,
      but now exists in the codebase.

--- src/minibatching.jl
+++ src/minibatching.jl
@@ -20,9 +20,6 @@
 # KernelFlows.jl. It works the same way as the optimizers.jl class,
 # defining a mutable struct for each minibatching method.
 
-using NearestNeighbors
-
-
 abstract type AbstractMinibatch end
 
 
@@ -30,29 +27,9 @@ const n_default = 64
 const κ_default = 3
 
 
-mutable struct RandomPartitions <: AbstractMinibatch
-    const ndata::Int
-    const n::Int # size of minibatch
-    const κ::Int # number of points to predict
-    const niter::Int # number of iterations
-    const all_s::Vector{Vector{Int}} # minibatch
-    i::Int
-end
-
-
-function RandomPartitions(X::AbstractArray;
-                          n::Int = n_default, niter::Int = 1000, κ::Int = κ_default)
-    ndata = size(X)[1]
-    all_s = get_random_partitions(ndata, n, niter)
-    RandomPartitions(ndata, n, κ, niter, all_s, 1)
-end
-
-
-function minibatch(B::RandomPartitions, λ::Vector{T}) where T <: Real
-    s = B.all_s[B.i]
-    B.i = B.i + 1
-    s
-end
+include("mb_covariance.jl")
+include("mb_multicenter.jl")
+include("mb_randompartitions.jl")
 
 
 mutable struct MulticenterMinibatch <: AbstractMinibatch
@@ -101,7 +78,6 @@ function update_samples(B::MulticenterMinibatch, λ::Vector{T}) where T <: Real
         m = ndata - length(s_nbs) # number of points to sample global data from
         nglobal = B.n - length(s_nbs)
         r1 = randperm(m)[1:nglobal] # global data indexes
-
         r2 = setdiff(1:ndata, s_nbs)[r1]
         push!(epoch_s, vcat(s_nbs, r2))
     end
@@ -110,42 +86,32 @@ end
 
 
 """Test and plot results to verify that minibatching works as intended."""
-function test_Multicenter(; p = Plots.plot())
+function test_Multicenter(; p = Plots.plot(), λ = rand(2))
     X = rand(1000,2)
     λ = rand(2)
     ll = round.(λ; sigdigits = 2)
-    κ = 4
-    B = KernelFlows.MulticenterMinibatch(X; n = 50, κ, niter = 100) # , nnb = 6)
+    κ = 5
+
+    B = KernelFlows.MulticenterMinibatch(X; n = 64, κ, niter = 100 , nnb = 6)
     s = KernelFlows.minibatch(B, λ)
     nnb_tot = B.nnb * κ
 
+    cs = palette(:tab10)
+    c1 = cs[4]
+    c2 = cs[2]
+    c3 = cs[3]
+
+    ms = 7
+
     Plots.plot!(p, title = "x-y scaling parameters: $(ll[1]), $(ll[2])")
-    Plots.scatter!(p, X[s[1:κ],1], X[s[1:κ],2], label = "Minibatch / centers")
-    Plots.scatter!(p, X[s[κ+1:κ + 1 + nnb_tot],1], X[s[κ+1:κ + 1 + nnb_tot],2], label = "Minibatch / neighbors")
-    Plots.scatter!(p, X[s[κ+1+nnb_tot:end],1], X[s[κ+1+nnb_tot:end],2], label = "Minibatch / others")
+    Plots.scatter!(p, X[s[κ+1+nnb_tot:end],1], X[s[κ+1+nnb_tot:end],2], label = "All predictor points", alpha = 0.5, z_order = 1, color = cs[1], ms = ms)
+    Plots.scatter!(p, X[s[1:κ],1], X[s[1:κ],2], label = "Predicted points", alpha = 1., color = c1, z_order = 2, ms = ms)
     sdiff = setdiff(1:1000,s)
-    Plots.scatter!(p, X[sdiff,1], X[sdiff,2], label = "Data not in minibatch", alpha = 0.1, color = :gray)
+    Plots.scatter!(p, X[s[κ+1:κ + 1 + nnb_tot],1], X[s[κ+1:κ + 1 + nnb_tot],2], label = "Minibatch / neighbors", alpha = 0.7, z_order = 2, color = c3)
+    Plots.scatter!(p, X[s[κ+1:κ + 1 + nnb_tot],1], X[s[κ+1:κ + 1 + nnb_tot],2], label = false, alpha = 0.5, z_order = 2, color = cs[1], ms = ms)
+    Plots.scatter!(p, X[sdiff,1], X[sdiff,2], label = "Not in minibatch", alpha = 0.2, color = :gray, ms = ms)
     Plots.plot!(p, xticks = [], yticks = [])
 end
-
-"""Test and plot results to verify that minibatching works as intended."""
-function test_RandomPartitions(; p = Plots.plot())
-    X = rand(1000,2)
-    λ = rand(2)
-    κ = 3
-    B = RandomPartitions(X .* λ'; n = 100, niter = 1000, κ)
-    s1 = minibatch(B, ones(2))
-    s2 = minibatch(B, ones(2))
-    s3 = minibatch(B, ones(2))
-    Plots.scatter!(p, X[s1,1], X[s1,2], label = "First minibatch")
-    Plots.scatter!(p, X[s2,1], X[s2,2], label = "Second minibatch")
-    Plots.scatter!(p, X[s3,1], X[s3,2], label = "Third minibatch")
-    sdiff = setdiff(1:1000,vcat(s1, s2, s3))
-    Plots.plot!(p, xticks = [], yticks = [])
-
-    Plots.scatter!(p, X[sdiff,1], X[sdiff,2], label = "Data not in minibatches 1-3", alpha = 0.1)
-end
-
 
 
 function get_minibatcher(mbalg::Symbol, X::AbstractArray{T};
